5c0802f4003d6a62c9ebd66224237c22e5abe43e
Fumiaki MATSUSHIMA <macchima23@gmail.com>
2013-05-10 22:39:51 +0900

    Change terminal

--- dot.config/awesome/rc.lua
+++ dot.config/awesome/rc.lua
@@ -41,7 +41,7 @@ end
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "terminator"
+terminal = "gnome-terminal"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
