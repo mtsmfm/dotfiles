af205b3753d8081004363fd24d0c30725ef5f0ff
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-06-07 15:35:31 +0900

    [awesome] unset wallpaper

--- config/awesome/theme.lua
+++ config/awesome/theme.lua
@@ -72,8 +72,6 @@ theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/def
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "/usr/share/themes/Adwaita/backgrounds/morning.jpg"
-
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
 theme.layout_fairv = "/usr/share/awesome/themes/default/layouts/fairvw.png"
